8d2563b1ba8f963d43bf433cce80fcda5104fd8f
espriter <esprit@espriter.net>
2018-03-30 18:09:56 +0900

    2018-03-30 update

--- espriter_app08_db/src/main/java/pack2/BusinessInter.java
+++
@@ -1,5 +0,0 @@
-package pack2;
-
-public interface BusinessInter {
-	void dataList();
-}
--- espriter_app08_db/src/main/java/pack2/SangpumInter.java
+++
@@ -1,8 +0,0 @@
-package pack2;
-
-import java.util.List;
-import org.springframework.dao.DataAccessException;
-
-public interface SangpumInter {
-	List<SangpumDto> selectList() throws DataAccessException;
-}
